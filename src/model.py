538170e64541df1814acf15bc986b93fa86f1ee9
Deleuca <ryanmaroun3@gmail.com>
2026-03-30 16:53:35 +0200

    * added function signature

--- src/model.py
+++ src/model.py
@@ -2,3 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd 
 import numpy as np
+
+def random_forest(X,y):
+    accuracy = 0
+    return accuracy
